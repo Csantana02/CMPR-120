97c88559f4bf3b23edf2e93a661e278301f95cfe
Christopher Santana <christopher.santana22@outlook.com>
2023-11-05 16:33:42 -0800

    re-factored and created a function

--- Exercise 7- Payroll Processing Files/Exercise 7- Payroll Processing Files.cpp
+++ Exercise 7- Payroll Processing Files/Exercise 7- Payroll Processing Files.cpp
@@ -17,64 +17,25 @@ const double FEDERAL_TAX_UNDER_1150 = 0.20;
 const double FEDERAL_TAX_OVER_1150 = 0.25;
 const double FEDERAL_TAX_LIMIT = 1150.0;
 
+string getFileName();
+
 int main()
 {
 	// variable declarations
-	string inputFileName;
-	string outputFileName;
 	ifstream inputFile;
 	ofstream outputFile;
 
 	// get the name of the input file from the user
-	do
-	{
-		// prompt the user to enter the file name
-		cout << "Enter the name of the input payroll file: " << endl;
-		getline(cin, inputFileName);
-		cin.ignore(99, '\n');
-
-		// validate the input
-		if (inputFileName.length() == 0)
-		{
-			cout << "Error: File name cannot be empty. Please try again." << endl;
-			continue;
-		}
-
-		// program is opening the input file
-		inputFile.open(inputFileName);
-		if (!inputFile)
-		{
-			cout << "Error: Unable to open the input file. Please try again." << endl;
-			continue;
-		}
-
-		break; // valid input for the file name
-	} while (true);
+	// prompt the user to enter the file name
+	cout << "Enter the name of the input payroll file: " << endl;
+	string inputFileName = getFileName();
 
 	// get the name of the output file from the user
-	do
-	{
-		// prompt the user to enter the payroll file
-		cout << "Enter the name of the payroll report file: ";
-		getline(cin, outputFileName);
-		cin.ignore(99, '\n');
+	cout << "Enter the name of the payroll report file: " << endl;
+	string outputFileName = getFileName();
 
-		//validate the input
-		if (outputFileName.length() == 0)
-		{
-			cout << "Error: File name cannot be empty. Please try again." << endl;
-			continue;
-		}
-
-		outputFile.open(outputFileName);
-		if (!outputFile)
-		{
-			cout << "Error: Unable to open the output file. Please try again." << endl;
-			continue;
-		}
-
-		break; // Valid output file name
-	} while (true);
+	inputFile.open(inputFileName);
+	outputFile.open(outputFileName);
 
 	// program is setting up numerical formating
 	cout << setprecision(2) << fixed << showpoint;
@@ -93,7 +54,7 @@ int main()
 	outputFile << setw(10) << "TOTAL FICA" << setw(10) << "FEDERAL TAX";
 	outputFile << setw(15) << "NET PAY" << endl;
 
-	// program is prcoessing the file
+	// program is processing the file
 	while (true)
 	{
 		string employeeID;
@@ -119,7 +80,7 @@ int main()
 		// calculating gross pay 
 		grossPay = (hoursWorked <= 40) ? hourlyRate * hoursWorked : (hourlyRate * 40) + (hourlyRate * 2 * (hoursWorked - 40));
 
-		// calculating the witholding
+		// calculating the withholding
 		socialSecurityTax = grossPay * SOCIAL_SECURITY_TAX;
 		medicareTax = grossPay * MEDICARE_TAX;
 		totalFICATax = socialSecurityTax + medicareTax;
@@ -148,4 +109,38 @@ int main()
 	outputFile.close();
 
 	return 0;
+}
+
+string getFileName()
+{
+	string inputFileName;
+	ifstream inputFile;
+
+	do
+	{
+		getline(cin, inputFileName);
+		cin.ignore(99, '\n');
+
+		// validate the input
+		if (inputFileName.length() == 0)
+		{
+			cout << "Error: File name cannot be empty. Please try again." << endl;
+			continue;
+		}
+
+		// program is opening the input file
+		inputFile.open(inputFileName);
+		if (!inputFile)
+		{
+			cout << "Error: Unable to open the file. Please try again." << endl;
+			continue;
+		}
+
+		break; // valid input for the file name
+	} while (true);
+
+	// close the file
+	inputFile.close();
+
+	return inputFileName;
 }
